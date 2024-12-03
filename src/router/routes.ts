25335a25cbe9c76e0f6540d54a750f1c62a03148
Joao Nuno Mabjaia <94103183+joaonunomabjaia@users.noreply.github.com>
2024-12-03 10:02:34 +0200

    Project_Rebasing

---
+++ src/router/routes.ts
@@ -0,0 +1,27 @@
+import { RouteRecordRaw } from 'vue-router';
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    redirect: '/login', // Redirect the root path to the login page
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('pages/LoginPage.vue'),
+  },
+  {
+    path: '/main',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+  },
+
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: '/:catchAll(.*)*',
+    component: () => import('pages/ErrorNotFound.vue'),
+  },
+];
+
+export default routes;
